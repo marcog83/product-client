a9a12e389784b88544a728765e81ba786024c44d
gobbimar <marco.gobbi@adidas.com>
2020-05-21 18:20:28 +0200

    deploy

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
-export default {
+ 
+
+  const dev = {
     s3: {
       REGION: "us-east-1",
       BUCKET: "product-list-uploads"
@@ -13,4 +15,32 @@ export default {
       APP_CLIENT_ID: "619ns088gk9k07u3elkr0ka5bv",
       IDENTITY_POOL_ID: "us-east-1:98a09952-f76b-42e1-867d-d998aae1cb21"
     }
+  };
+  
+  const prod = {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "product-list-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://coxea1byv1.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_nh2YXQfCr",
+      APP_CLIENT_ID: "622a5d432qo9tnirr81iric8fs",
+      IDENTITY_POOL_ID: "us-east-1:5f8bb8ee-5de1-46a3-8fc1-22048d7a32ec"
+    }
+  };
+  
+  // Default to dev if not set
+  const config = process.env.REACT_APP_STAGE === 'prod'
+    ? prod
+    : dev;
+  
+  export default {
+    // Add common config values here
+    MAX_ATTACHMENT_SIZE: 5000000,
+    ...config
   };
