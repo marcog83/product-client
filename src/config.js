adf398e742ce25fde714e3b1f240b7c904bca38b
gobbimar <marco.gobbi@adidas.com>
2020-05-21 16:00:37 +0200

    homepage

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "product-list-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://uez93u1rbk.execute-api.us-east-1.amazonaws.com/dev"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_mflmGrRy6",
+      APP_CLIENT_ID: "619ns088gk9k07u3elkr0ka5bv",
+      IDENTITY_POOL_ID: "us-east-1:98a09952-f76b-42e1-867d-d998aae1cb21"
+    }
+  };
